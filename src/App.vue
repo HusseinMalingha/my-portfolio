fc8be374fde5aa25733185d2c8cf54a053a01e99
HusseinMalingha <husseinmalingha@gmail.com>
2023-06-19 14:29:19 +0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import MainCard from './components/MainCard.vue'
+</script>
+
+<template>
+  <main>
+    <MainCard />
+  </main>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+@media only screen and (max-width: 567px) {
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    background-image: url('vue-portfolio/src/assets/me.jpeg');
+  }
+}
+</style>
---
+++ src/components/PdfViewer.vue
@@ -0,0 +1,13 @@
+<template>
+  <PdfApp style="height: 70vh;" pdf="/pdf/Hussein_Malingha_CV.pdf"></PdfApp>
+</template>
+
+<script>
+import PdfApp from "vue3-pdf-app";
+import "vue3-pdf-app/dist/icons/main.css";
+export default {
+  components: {
+    PdfApp,
+  },
+};
+</script>
